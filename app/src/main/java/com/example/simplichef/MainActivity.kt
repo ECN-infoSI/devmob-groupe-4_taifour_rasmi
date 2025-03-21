9f6aaa463f713448662eb1a1d53fea38e45bd12e
kaoutar <kaoutarrasmi2002@gmail.com>
2025-03-21 22:46:10 +0100

    Ajout de l'activity pour le choix des catégories et amélioration de l'affichage de la première page avec des commentaires aussi

--- app/src/main/java/com/example/simplichef/MainActivity.kt
+++ app/src/main/java/com/example/simplichef/MainActivity.kt
@@ -1,23 +1,33 @@
 package com.example.simplichef
 
 import android.os.Bundle
+import android.text.style.BackgroundColorSpan
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -41,26 +51,50 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+/**
+ * Composable pour l'affichage du message de départ
+ */
 @Composable
 fun GreetingText(message: String, modifier: Modifier = Modifier) {
-    Row(
-        horizontalArrangement = Arrangement.Center,
-        modifier = modifier
-    ) {
         Text(
             text = message,
             fontSize = 30.sp,
-            lineHeight = 116.sp,
-            textAlign = TextAlign.Center
+            fontFamily = FontFamily.Cursive,
+            textAlign = TextAlign.Center,
+            modifier=Modifier
+        )
+
+}
+/**
+ * Composable pour le button
+ */
+@Composable
+fun StartingButton(textButton: String, modifier: Modifier = Modifier) {
+    Button(
+        onClick = {
+
+        },
+        modifier = modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .height(56.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Black,
+        ),
+    ) {
+        Text(
+            text =textButton,
+            fontFamily = FontFamily.Cursive,
         )
 
     }
 }
-
+/**
+ * Composable pour l'affichage toute la page avec le back, le message et le button
+ */
 @Composable
 fun GreetingImage(message: String, modifier: Modifier = Modifier) {
-    val image = painterResource(R.drawable.arriere_plan)
-    val image2 = painterResource(R.drawable.left)
+    val image = painterResource(R.drawable.tagliatelle1)
 
     Box(modifier) {
         Image(
@@ -68,23 +102,19 @@ fun GreetingImage(message: String, modifier: Modifier = Modifier) {
             contentDescription = null,
             contentScale = ContentScale.Crop,
         )
-        Row(
-            horizontalArrangement = Arrangement.Center,
-            modifier = modifier
-        ) {
-            Image(
-                painter = image2,
-                contentDescription = null,
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    .size(50.dp)
-            )
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 300.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally)
+            {
             GreetingText(
                 message = message,
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(8.dp)
             )
+            StartingButton("Allons-y!")
         }
     }
 
